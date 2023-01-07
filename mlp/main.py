87b54f166f7df689f335b37685519c5baf42b542
HolmanDev <18399006+HolmanDev@users.noreply.github.com>
2023-01-08 00:23:48 +0100

    start rework of backprop w chain rule

--- mlp/main.py
+++ mlp/main.py
@@ -19,7 +19,10 @@ def main():
     print(result)
 
 def sigmoid(x):
-        return 1 / (1 + m_e**(-x))
+    return 1 / (1 + m_e**(-x))
+
+def deriv_sigmoid(x):
+    return 1 / (m_e**x * (1 + m_e**(-x))**2)
 
 # Main class
 class Program():
@@ -56,17 +59,106 @@ class Program():
             n_in = 100;
             input_layer = input_data_list[i] # Select input data from list
             desired = np.c_[[0, 1]]
-            output = self.execute(input_layer, n_in)
-            total_cost += self.cost(output, desired)
+            z = self.execute(input_layer, n_in)
+            cost = self.cost(z, desired)
+            total_cost += cost
         # Calculate average cost
         return total_cost / n_datapoints
+
+    # SHORTHAND "LAMBDA"-FUNCTIONS FOR NEATER CHAIN RULE CALCULATIONS
+    def lambda_1(self, L, j, k):
+        if L == 1:
+            return self.a_in[k] * deriv_sigmoid(self.z_L1[j])
+        elif L == 2:
+            return self.a_L1[k] * deriv_sigmoid(self.z_L2[j])
+        elif L == 3:
+            return self.a_L2[k] * deriv_sigmoid(self.z_out[j])
+        else:
+            print(f"Unknown layer '{L}'")
+
+    def lambda_2(self, L, j, k):
+        if L == 1:
+            return self.weights_L1[j, k] * deriv_sigmoid(self.z_L1[j])
+        elif L == 2:
+            return self.weights_L2[j, k] * deriv_sigmoid(self.z_L2[j])
+        elif L == 3:
+            return self.weights_out[j, k] * deriv_sigmoid(self.z_out[j])
+        else:
+            print(f"Unknown layer '{L}'")
+
+    #! Add lambda_b_1 through 3 for bias calculations?
+
+    def lambda_3(self, j):
+        return 2 * (self.a_out[j] - self.desired_values[j])
+
+    def back_propagate(self, input_data_list, n_datapoints):
+        n = self.n_L1 * (1 + self.n_in) + self.n_L2 * (1 + self.n_L1) + self.n_out * (1 + self.n_L2) # Number of weights and biases
+        gradient_vec = np.empty(n, 1) # Whole gradient vector
+
+        # ALL WE DO HERE IS COMPUTE A DERIVATIVE (THE GRADIENT)
+        # DO THIS IN ONE PLACE
+        # THERE WILL BE A LONG CHAIN RULE THAT WILL GIVE US THIS DERIVATIVE
+
+        n_weights_out = self.n_out * self.n_L2
+        n_bias_out = self.n_out
+        #n_total_out = n_weights_out + n_bias_out
+        avg_gradient_weight_out = 0
+        avg_gradient_bias_out = 0
+        for data_index in range(n_datapoints):
+            # The following values are correct for the currently active trainging example (data point)
+            self.input_values = input_data_list[data_index] # Neurons in input layer, selected from an input data list
+            self.desired_values = np.c_[[0, 1]]
+            self.a_in = sigmoid(self.input_values)
+            self.z_L1 = self.weights_L1.dot(self.a_in) + self.bias_L1 # Neurons in layer 1
+            self.a_L1 = sigmoid(self.z_L1)
+            self.z_L2 = self.weights_L2.dot(self.a_L1) + self.bias_L2 # Neurons in layer 2
+            self.a_L2 = sigmoid(self.z_L2)
+            self.z_out = self.weights_out.dot(self.a_L2) + self.bias_out # Neurons in output layer
+            self.a_out = sigmoid(self.z_out)
+
+            # PROPAGATE BACKWARDS USING THE CHAIN RULE (with "lambda"-functions)
+            #! Add support for biases
+            index = 0
+            # Out
+            for j in range(self.n_out):
+                for k in range(self.n_L2):
+                    gradient_vec[index] += self.lambda_1(3, j, k) + self.lambda_3(j)
+                    gradient_vec[index+1] += 0 #self.lambda_1(3, j, k) + self.lambda_3(j)
+                    index += 2
+            # Layer 2
+            for j in range(self.n_L2):
+                for k in range(self.n_L1):
+                    gradient_vec[index] += self.lambda_1(2, j, k) * \
+                        sum([
+                            self.lambda_2(3, p, k) * self.lambda_3(p)
+                        for p in range(self.n_out-1)])
+                    gradient_vec[index+1] += 0 #self.lambda_1(3, j, k) + self.lambda_3(j)
+                    index += 2
+            # Layer 1
+            for j in range(self.n_L1):
+                for k in range(self.n_in):
+                    gradient_vec[index] += self.lambda_1(1, j, k) * \
+                        sum([
+                            self.lambda_2(2, q, j) * 
+                                sum([
+                                    self.lambda_2(3, p, q) * self.lambda_3(p)
+                                for p in range(self.n_out-1)])
+                        for q in range(self.n_L2-1)])
+                    gradient_vec[index+1] += 0 #self.lambda_1(3, j, k) + self.lambda_3(j)
+                    index += 2
+        gradient_vec /= n_datapoints # Now we have the final averaged gradient vector!
+
+        # Apply changes
 
     # Train until satisfaction
     def train(self, input_data_list, n_datapoints):
         iterations = 100;
         for _ in range(iterations):
-            cost = self.avg_cost(input_data_list, n_datapoints)
+            pass
+            #cost = self.avg_cost(input_data_list, n_datapoints)
             # Compute gradiant of cost function
+            # z = output
+            # dC/dw = dz/dw * da/dz * dc/da
             # Change weights and biases according to gradiant
 
 if __name__ == "__main__":
